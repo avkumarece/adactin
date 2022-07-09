e5a41ca9b5eb221f3f0a0ccba53c21b4208956db
avkumarece <avkumarece@gmail.com>
2022-07-09 16:48:20 +0530

    Ram update

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -6,6 +6,9 @@ public class Company extends Client {
 System.out.println("Company name is ABC");
 
 	}
+	private void ramMethod() {
+		System.out.println("hello");
+	}
 	public static void main(String[] args) {
 		Company a = new Company();
 		a.companyName();
